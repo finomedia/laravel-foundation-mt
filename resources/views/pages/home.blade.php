3270977f601aa196428ed65e6409df0fc961dcae
Fino Galindo <fino@cinsay.com>
2015-09-02 23:59:25 -0500

    the/art/soul

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 
 <div id="page">
     <div class="container">
-        <div style="font-family: Helvetica, Arial, sans-serif; font-size: 3rem; font-weight: bold;">art/soul</div>
+        <div style="font-family: Helvetica, Arial, sans-serif; font-size: 3rem; font-weight: bold;">the/art/soul</div>
         <div style="font-size: 1rem; font-family: Helvetica, Arial, sans-serif;">v1 - Updated Again</div>
     </div>
 </div>
