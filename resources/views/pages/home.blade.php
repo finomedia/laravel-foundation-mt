74a4e5e8ea33006811fe0bf1acb671dc80ef6c4e
Fino Galindo <fino@cinsay.com>
2015-09-02 23:05:12 -0500

    Update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,13 +2,11 @@
 
 @section('content')
 
-    <div id="page">
-        <div class="container">
-            <div class="content">
-                <div class="title">art/soul</div>
-                <div style="font-size: 1.25rem; font-weight: bold; font-family: Helvetica, Arial, sans-serif;">v1 - Updated Again</div>
-            </div>
-        </div>
+<div id="page">
+    <div class="container">
+        <div style="font-family: Helvetica, Arial, sans-serif; font-size: 3rem; font-weight: bold;">art/soul</div>
+        <div style="font-size: 1rem; font-family: Helvetica, Arial, sans-serif;">v1 - Updated Again</div>
     </div>
+</div>
 
 @endsection
