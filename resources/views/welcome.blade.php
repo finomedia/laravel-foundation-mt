9af160d3a19a245b178adfad39a98b2c82d766c7
Fino Galindo <fino@cinsay.com>
2015-09-02 21:46:25 -0500

    Saving CSS link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
 
         <style>
             html, body {
