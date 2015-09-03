211bd6d89a3bcdb2eba3fc29f5b8402fb048a974
Fino Galindo <fino@cinsay.com>
2015-09-02 22:51:41 -0500

    added home page template

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('content')
+
+    <div id="page">
+        <div class="container">
+            <div class="content">
+                <div class="title">art/soul</div>
+                <div style="font-size: 1.25rem; font-weight: bold; font-family: Helvetica, Arial, sans-serif;">v1 - Updated Again</div>
+            </div>
+        </div>
+    </div>
+
+@endsection
