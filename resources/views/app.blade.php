1390a34720743552e29a6f973e344fe6bbb8fae1
Fino Galindo <fino@cinsay.com>
2015-09-02 23:21:22 -0500

    Update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-
         <title>art/soul - a finomedia idea.</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="{{ asset('/css/foundation.css') }}" rel="stylesheet">
         <script src="{{ asset('/js/vendor/modernizr.js') }}"></script>
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-
     </head>
     <body>
         @yield('content')
